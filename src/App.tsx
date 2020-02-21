2aa0d1a663732819b5711cf3d4f2d0051e731f48
Toh Hong Xiang <hongxiang@ieplsg.com>
2020-02-21 11:38:35 +0800

    Dragging widget clones instead of removes from list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.scss'
 import Editor from './components/Editor'
 
--- src/components/WidgetList.tsx
+++ src/components/WidgetList.tsx
@@ -25,17 +25,25 @@ export default function WidgetList() {
     return (
         <div className={styles.widgetContainer}>
             <Droppable droppableId="widget-list" isDropDisabled={true}>
-                {provided => <ul className={styles.widgetList} {...provided.droppableProps} ref={provided.innerRef}>
+                {(provided, snapshot) => <ul className={styles.widgetList} ref={provided.innerRef} {...provided.droppableProps}>
                     {availableWidgets.map((widget: any, index) => (
-                        <li>
-                        <Draggable draggableId={`${widget.id}-widget`} index={index} key={widget.id} >
+                        <Draggable draggableId={`${widget.id}-widget`} index={index} key={widget.id}>
                             {(provided, snapshot) =>  (
-                                <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-                                    <div className={`${styles.widgetButton} ${snapshot.isDragging && styles.isDragging}`}>{widget.type}</div>
-                                </div>
+                                <>
+                                    <li 
+                                        ref={provided.innerRef} 
+                                        {...provided.dragHandleProps} 
+                                        {...provided.draggableProps} 
+                                        style={provided.draggableProps.style} 
+                                        className={`${styles.widgetButton} ${snapshot.isDragging && styles.isDragging}`}>
+                                        {widget.type}
+                                    </li>
+                                    {snapshot.isDragging && (
+                                        <li className={`${styles.widgetButton} ${styles.widgetClone}`}>{widget.type}</li>
+                                    )}
+                                </>
                             )}
                         </Draggable>
-                        </li>
                     ))}
                     {provided.placeholder}
                 </ul>}
